423ff3198a29d712f45d827e6c112a8e9f8cf3fe
Paulo H. Leocadio <22998694+paulohl@users.noreply.github.com>
2023-04-04 17:28:29 -0400

    Add files via upload

---
+++ src/RCPW100C.cbl
@@ -0,0 +1,16 @@
+       FD  RCD100C.
+       01  REG-RCD100C.
+           05  CHAVE-ALBUM-REC-C.
+               10  ALBUM-REC-C      PIC 9(08).
+           05  QPORTA-RETRATO-REC-C PIC 9(02).
+           05  QPENDRIVE-REC-C      PIC 9(02).
+           05  QVIDEO-HD-REC-C      PIC 9(02).
+           05  QREVISTA-REC-C       PIC 9(02).
+           05  QCALENDARIO-REC-C    PIC 9(02).
+           05  PED-EXT-BOOK-C       PIC 9(01).
+           05  PED-EXT-PORTA-RET-C  PIC 9(01).
+           05  PED-EXT-PENDRIVE-C   PIC 9(01).
+           05  PED-EXT-VIDEO-HD-C   PIC 9(01).
+           05  PED-EXT-REVISTA-C    PIC 9(01).
+           05  PED-EXT-CALEND-C     PIC 9(01).
+           05  FILLER               PIC X(94).
